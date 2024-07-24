4f1e4935b73229b4e61d63bf007b2837db616062
rakau19 <149530235+rakau19@users.noreply.github.com>
2024-07-24 12:11:23 -0700

    Create main.tf

---
+++ modules/vnet/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.113.0"
+    }
+  }
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "vnets" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "subnets" {
+  for_each             = var.subnets
+  name                 = each.value.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnets.name
+  address_prefixes     = each.value.address_prefixes
+
+}

91982563fa6325c72a89130bb90d23b608093b01
rakau19 <149530235+rakau19@users.noreply.github.com>
2024-07-24 12:36:11 -0700

    Update main.tf

--- Connectivity/main.tf
+++ Connectivity/main.tf
@@ -1,22 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 module "vnet" {
   source              = "../modules/vnet"
-  vnet_name           = var.vnet_name
   location            = var.location
+  vnet_name           = var.vnet_name
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
   subnets             = var.subnets
+  tags                = var.tags
 
-}
-
-module "firewall" {
-  source              = "../modules/firewall"
-  firewall_name       = var.firewall_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  pip_fw_name         = var.pip_fw_name
-  subnet_id           = module.vnet.subnet_ids[0]
-
-  depends_on = [module.vnet]
+  depends_on = [azurerm_resource_group.rg]
 }
 
 module "bastion" {
@@ -26,21 +22,25 @@ module "bastion" {
   resource_group_name = var.resource_group_name
   ip_config_name      = var.ip_config_name
   pip_bastion_name    = var.pip_bastion_name
-  subnet_id           = module.vnet.subnet_ids[1]
+  subnet_id           = module.vnet.subnets_ids[1]
+  tags                = var.tags
 
-  depends_on = [ module.vnet ]
+  depends_on = [module.vnet]
 }
 
-module "appgw" {
-  source              = "../modules/AppGW"
-  resource_group_name = var.resource_group_name
-  vnets               = var.vnet_name
-  location            = var.location
-  pip_appgw_name      = var.pip_appgw_name
-  appgw_name          = var.appgw_name
-  ip_gw_config        = var.ip_gw_config
-  subnets             = module.vnet.subnet_ids[2]
-  address_prefixes    = var.address_prefixes
+module "AppGW" {
+  source                  = "../modules/AppGW"
+  resource_group_name     = var.resource_group_name
+  vnet_name               = var.vnet_name
+  location                = var.location
+  pip_appgw_name          = var.pip_appgw_name
+  appgw_name              = var.appgw_name
+  ip_gw_config            = var.ip_gw_config
+  snet_appgw              = var.snet_appgw
+  appgw_address_prefixes  = var.appgw_address_prefixes
+  ssl_certificate_name_kv = var.ssl_certificate_name_kv
+  fqdns_name              = var.fqdns_name
+  tags                    = var.tags
 
-  depends_on = [ module.vnet ]
+  depends_on = [module.vnet]
 }

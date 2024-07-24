6402a1cd07a3a93b60948f7473b232ae952aad6b
rakau19 <149530235+rakau19@users.noreply.github.com>
2024-07-24 12:36:45 -0700

    Update var.tf

--- Connectivity/var.tf
+++ Connectivity/var.tf
@@ -1,73 +1,93 @@
 variable "location" {
-  description = "The location for all resources."
   type        = string
+  description = "The location for all resources."
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group."
   type        = string
+  description = "The name of the resource group."
 }
 
 variable "vnet_name" {
-  description = "The name of the virtual network."
   type        = string
+  description = "The virtual network name"
 }
 
 variable "address_space" {
-  description = "The address space for the virtual network."
   type        = list(string)
+  description = "The address space"
 }
 
 variable "subnets" {
-  description = "The list of subnets."
-  type = list(object({
-    name            = string
-    address_prefixes = list(string)
+  description = "The map of Subnets"
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    vnet_name           = string
+    address_prefixes    = list(string)
   }))
 }
 
-variable "firewall_name" {
-  description = "The name of the Firewall"
-  type = string
-}
-
-variable "address_prefixes" {
-  description = "The address prefixes"
-  type = list(string)
-
-}
-
-variable "pip_fw_name" {
-  description = "The public IP firewall name"
-  type = string
+variable "tags" {
+  type        = map(string)
+  description = "The resource tag"
 }
 
 variable "bastion_name" {
   description = "The bastion name"
-  type = string
+  type        = string
 }
 
 variable "ip_config_name" {
   description = "The bastion IP config name"
-  type = string
+  type        = string
 }
 
 variable "pip_bastion_name" {
   description = "The public IP bastion name"
-  type = string
+  type        = string
 }
 
 variable "pip_appgw_name" {
   description = "The public IP application gateway name"
-  type = string
+  type        = string
+}
+
+variable "snet_appgw" {
+  description = "The list of sub."
+  type        = string
 }
 
 variable "appgw_name" {
   description = "The application gateway name"
-  type = string
+  type        = string
 }
 
 variable "ip_gw_config" {
   description = "The IP application gateway config name"
-  type = string
+  type        = string
 }
+
+variable "appgw_address_prefixes" {
+  description = "The address prefixes"
+  type        = list(string)
+}
+
+variable "ssl_certificate_name_kv" {
+  description = "The SSL certificate name in Key vault"
+  type        = string
+}
+
+variable "fqdns_name" {
+  description = "The fully qualified domain name"
+  type        = set(string)
+}
+#variable "peering_name" {
+#  description = "The vnet peering name"
+#  type        = string
+#}
+
+#variable "remote_vnet_id" {
+#  description = "The full Azure resource ID of the remote virtual network for peering."
+#  type        = string
+#}

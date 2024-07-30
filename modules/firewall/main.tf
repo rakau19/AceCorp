3c695e8e12983c857f3c8933c6f76e43e119d6be
rakau19 <149530235+rakau19@users.noreply.github.com>
2024-07-30 08:16:19 -0700

    Update main.tf
    revised #2

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,5 +1,14 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnets" {
+  name                = var.vnet_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
 data "azurerm_subnet" "subnets" {
-  name = var.subnets
+  name = var.fw_subnet
   resource_group_name = var.resource_group_name
   virtual_network_name = var.vnet_name
 }
@@ -21,7 +30,7 @@ resource "azurerm_firewall" "firewall" {
 
   ip_configuration {
     name                 = "ip-config-AzureFirewall"
-    subnet_id            = data.azurerm_subnet.subnets.id
+    subnet_id            = var.subnets_id
     public_ip_address_id = azurerm_public_ip.pip_fw.id
   }
 

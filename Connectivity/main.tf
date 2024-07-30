8eb00185017706ae3e779480f941c9de8c7950cf
rakau19 <149530235+rakau19@users.noreply.github.com>
2024-07-30 08:05:39 -0700

    Update main.tf
    revised #2

--- Connectivity/main.tf
+++ Connectivity/main.tf
@@ -22,25 +22,67 @@ module "bastion" {
   resource_group_name = var.resource_group_name
   ip_config_name      = var.ip_config_name
   pip_bastion_name    = var.pip_bastion_name
-  subnet_id           = module.vnet.subnets_ids[1]
+  subnets_id          = module.vnet.subnets_ids[1]
   tags                = var.tags
 
   depends_on = [module.vnet]
 }
 
-module "AppGW" {
+module "appgw" {
   source                  = "../modules/AppGW"
   resource_group_name     = var.resource_group_name
   vnet_name               = var.vnet_name
   location                = var.location
+  subnets_id              = module.vnet.subnets_ids[0]
   pip_appgw_name          = var.pip_appgw_name
   appgw_name              = var.appgw_name
+  appgw_subnet            = var.appgw_subnet
   ip_gw_config            = var.ip_gw_config
-  snet_appgw              = var.snet_appgw
-  appgw_address_prefixes  = var.appgw_address_prefixes
   ssl_certificate_name_kv = var.ssl_certificate_name_kv
   fqdns_name              = var.fqdns_name
   tags                    = var.tags
 
   depends_on = [module.vnet]
+}
+
+module "firewall" {
+  source              = "../modules/firewall"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  vnet_name           = var.vnet_name
+  firewall_name       = var.firewall_name
+  fw_subnet           = "AzureFirewallSubnet"
+  pip_fw_name         = var.pip_fw_name
+  subnets_id          = module.vnet.subnets_ids[2]
+  tags                = var.tags
+
+  depends_on = [module.vnet]
+}
+
+module "peering" {
+  source              = "../modules/peering"
+  peering_name        = var.peering_name
+  resource_group_name = var.resource_group_name
+  vnet_name           = var.vnet_name
+  remote_vnet_id      = var.remote_vnet_id
+
+  depends_on = [module.vnet]
+}
+
+module "rt_appgw" {
+  source = "../modules/routetable"
+  location = var.location
+  resource_group_name = var.resource_group_name
+  route_tables = var.route_tables
+  firewall_ip = module.firewall.firewall_ip
+  tags = var.tags
+
+  depends_on = [ module.firewall ]
+}
+
+resource "azurerm_subnet_route_table_association" "appgw" {
+  subnet_id      = module.vnet.subnets_ids[0]
+  route_table_id = module.rt_appgw.route_table_ids[0]
+
+  depends_on = [module.rt_appgw]
 }

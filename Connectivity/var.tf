a71921061b5f0c8e12b38f0a429f8ca528df4c2f
rakau19 <149530235+rakau19@users.noreply.github.com>
2024-07-30 08:06:24 -0700

    Update var.tf
    revised #2

--- Connectivity/var.tf
+++ Connectivity/var.tf
@@ -1,11 +1,11 @@
 variable "location" {
   type        = string
-  description = "The location for all resources."
+  description = "The location for all resources"
 }
 
 variable "resource_group_name" {
   type        = string
-  description = "The name of the resource group."
+  description = "The name of the resource group"
 }
 
 variable "vnet_name" {
@@ -30,64 +30,80 @@ variable "subnets" {
 
 variable "tags" {
   type        = map(string)
-  description = "The resource tag"
+  description = "The map resource tags"
 }
 
 variable "bastion_name" {
-  description = "The bastion name"
   type        = string
+  description = "The bastion name"
 }
 
 variable "ip_config_name" {
-  description = "The bastion IP config name"
   type        = string
+  description = "The bastion IP config name"
 }
 
 variable "pip_bastion_name" {
+  type        = string
   description = "The public IP bastion name"
-  type        = string
-}
-
-variable "pip_appgw_name" {
-  description = "The public IP application gateway name"
-  type        = string
-}
-
-variable "snet_appgw" {
-  description = "The list of sub."
-  type        = string
 }
 
 variable "appgw_name" {
-  description = "The application gateway name"
   type        = string
+  description = "The application gateway name"
+}
+
+variable "appgw_subnet" {
+  type = string
+}
+
+variable "pip_appgw_name" {
+  type        = string
+  description = "The public IP application gateway name"
 }
 
 variable "ip_gw_config" {
+  type        = string
   description = "The IP application gateway config name"
-  type        = string
-}
-
-variable "appgw_address_prefixes" {
-  description = "The address prefixes"
-  type        = list(string)
-}
-
-variable "ssl_certificate_name_kv" {
-  description = "The SSL certificate name in Key vault"
-  type        = string
 }
 
 variable "fqdns_name" {
-  description = "The fully qualified domain name"
   type        = set(string)
+  description = "The fully qualified domain name"
 }
-#variable "peering_name" {
-#  description = "The vnet peering name"
-#  type        = string
-#}
 
-#variable "remote_vnet_id" {
-#  description = "The full Azure resource ID of the remote virtual network for peering."
-#  type        = string
-#}
+variable "ssl_certificate_name_kv" {
+  type        = string
+  description = "The SSL certificate name in Key vault"
+}
+
+variable "firewall_name" {
+  type        = string
+  description = "The name of the Firewall"
+}
+
+variable "pip_fw_name" {
+  type        = string
+  description = "The public IP firewall name"
+}
+
+variable "route_tables" {
+  description = "Map of route tables to create"
+  type = map(object({
+    name                   = string
+    route_name             = string
+    address_prefix         = string
+    next_hop_type          = string
+    next_hop_in_ip_address = string
+  }))
+}
+
+variable "peering_name" {
+  type        = string
+  description = "The vnet peering name"
+}
+
+variable "remote_vnet_id" {
+  type        = string
+  description = "The full Azure resource ID of the remote virtual network for peering"
+}
